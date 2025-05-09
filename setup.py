83868cd7115c8e2525a4617963f2a0ae06df3907
Thomas Holterbach <tholterbach@unistra.fr>
2025-05-09 14:04:35 +0000

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pybgproutesapi",
-    version="0.1.0",
+    version="0.1.2",
     description="Python bindings for the bgproutes.io API",
     author="Thomas Holterbach and Thomas Alfroy",
     author_email="contact@bgproutes.io",
