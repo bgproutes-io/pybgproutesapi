042c3c9c0bc6bca0c410d7ceed8ce492ed1ddadf
Thomas Holterbach <tholterbach@unistra.fr>
2025-05-14 01:41:03 +0000

    Added the exemple in the RIPE 90

---
+++ examples/ripe_90.py
@@ -0,0 +1,13 @@
+from pybgproutesapi import vantage_points, updates
+
+vps = vantage_points(source=["bgproutes.io"])
+
+for vp in vps:
+    vp_upd = updates(
+        vp_ip=vp,
+        start_date="2025-05-14T00:00:00",
+        end_date="2025-05-15T00:00:00",
+        aspath_regexp="^3333 | 3333 | 3333$"
+    )
+
+    print (len(vps), vp, vp_upd)
